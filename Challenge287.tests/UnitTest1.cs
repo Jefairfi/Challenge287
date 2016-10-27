cf5932875ab39ddc3a4af622ff953f644fd7fd64
Jefairfi@gmail.com <Arcti311>
2016-10-26 19:28:45 -0700

    Initial Commit

---
+++ Challenge287.tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Challenge287.tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InvalidInputs()
+        {
+            Assert.AreNotEqual(0, Program.kaprekar(0));
+            Assert.AreNotEqual(0, Program.kaprekar(1111));
+            Assert.AreNotEqual(0, Program.kaprekar(1118789791));
+            Assert.AreNotEqual(0, Program.kaprekar(-89791));
+        }
+    }
+}
---
+++ Challenge287/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge287
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxValue = (int?)0;
+            for (int i = 0; i < 9999; i++)
+            {
+                var result = kaprekar(i);
+                if (result > maxValue)
+                {
+                    maxValue = result;
+                }
+            }
+        }
+
+        public static int largest_digit(int input)
+        {
+            var maxChar = input.ToString().Max();
+            return int.Parse(maxChar.ToString());
+        }
+
+        public static int descending_digits(int input)
+        {
+            var orderedList = input.ToString().OrderByDescending(x => x);
+            return int.Parse(string.Join("", orderedList).PadRight(4, '0'));
+        }
+
+        public static int ascending_digits(int input)
+        {
+            var ascendingList = input.ToString().OrderBy(x => x);
+            return int.Parse(string.Join("", ascendingList));
+        }
+
+        public static int? kaprekar(int input)
+        {
+            var cnt = 0;
+            if (input > 0 && input < 10000)
+            {
+                return null;
+            }
+            while (input != 6174)
+            {
+                cnt++;
+                input = descending_digits(input) - ascending_digits(input);
+                if (input == 0)
+                {
+                    return null;
+                }
+            }
+            return cnt;
+        }
+    }
+}
